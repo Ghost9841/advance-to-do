cfcca0835130b2a2afe7cb74187dd3c3512169b1
Asis423 <ashishsubedi423@gmail.com>
2024-10-06 18:33:47 +0545

    main UI changed , sign-in, sign-up done, Used own Credentials for sign-n and sign-up not much done of UI for sign-in sign-up added some authproviders {added NEXTAUTHJS} dones the works for email password verifications and exisitng users and new users

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,21 +1,31 @@
 'use client'
 
-
 import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Component() {
   const { data: session } = useSession()
   if (session) {
     return (
       <>
         Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        <button 
+          className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
+          onClick={() => signOut()}
+        >
+          Sign out
+        </button>
       </>
     )
   }
   return (
     <>
       Not signed in <br />
-      <button className="bg-blue-500 text-white px-4 py-2 m-4 rounded-md" onClick={() => signIn()}>Sign in</button>
+      <button 
+        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
+        onClick={() => signIn()}
+      >
+        Sign in
+      </button>
     </>
   )
 }
